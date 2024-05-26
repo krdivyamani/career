ffe797257d4de918417c12752196e1e353bbcbe0
krdivyamani <132926541+krdivyamani@users.noreply.github.com>
2024-05-26 15:22:55 +0000

    Added jobitem.html template

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': "ML Engineer",
+    'salary': 100000
+}, {
+    'id': 2,
+    'title': "Robotics Engineer",
+    'salary': 150000
+}, {
+    'id': 3,
+    'title': "CV Engineer",
+    'salary': 1100000
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS)
+
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
